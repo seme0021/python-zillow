d1856e80155604f64fb5d613675ea0e4dcbc40de
Mikhail Semeniuk <msemeniuk@truecar.com>
2016-01-06 21:28:12 -0800

    added tests to places

--- tests/test_valuation.py
+++ tests/test_valuation.py
@@ -12,11 +12,10 @@ class TestGetSearchResult(unittest.TestCase):
         data = xmltodict.parse(RAW_XML)
 
         place = Place()
-        place.set_data(data)
+        place.set_data(data.get('SearchResults:searchresults', None)['response']['results']['result'])
 
         self.assertEqual("2100641621", place.zpid)
         self.assertEqual(1723665, place.zestiamte.amount)
-
 
     def test_zestimate(self):
         RAW_XML = ""
@@ -26,7 +25,36 @@ class TestGetSearchResult(unittest.TestCase):
         data = xmltodict.parse(RAW_XML)
 
         place = Place()
-        place.set_data(data)
+        place.set_data(data.get('Zestimate:zestimate', None)['response'])
 
         self.assertEqual("2100641621", place.zpid)
         self.assertEqual(1723665, place.zestiamte.amount)
+
+    def test_getcomps_principal(self):
+        RAW_XML = ""
+        with open('./testdata/get_comps.xml', 'r') as f:
+            RAW_XML = ''.join(f.readlines())
+
+        data = xmltodict.parse(RAW_XML)
+
+        place = Place()
+        place.set_data(data.get('Comps:comps')['response']['properties']['principal'])
+
+        self.assertEqual("2100641621", place.zpid)
+
+    def test_getcomps_comps(self):
+        RAW_XML = ""
+        with open('./testdata/get_comps.xml', 'r') as f:
+            RAW_XML = ''.join(f.readlines())
+
+        data = xmltodict.parse(RAW_XML)
+        comps = data.get('Comps:comps')['response']['properties']['comparables']['comp']
+
+        comp_places = []
+
+        for datum in comps:
+            place = Place()
+            place.set_data(datum)
+            comp_places.append(place)
+
+        self.assertEqual(10, len(comp_places))
